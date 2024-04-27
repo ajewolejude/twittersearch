82730cfb22217167310b3a74fdea9354ab57f2d3
Ajewole Adedamola Jude <43986061+ajewolejude@users.noreply.github.com>
2024-04-27 15:57:15 +0200

    Update tweets.php

--- tweets.php
+++ tweets.php
@@ -3,10 +3,10 @@ require "lib/twitteroauth/autoload.php";
 
 use Abraham\TwitterOAuth\TwitterOAuth;
  
-define('CONSUMER_KEY', 'H0rOip9Gc5lWfBkwsbAHikHfS');
-define('CONSUMER_SECRET', 'zMqgOnV6z3lDYTd3khcWtJ0Eyke2PeNmzhz3yPdz3aDUZKKyxT');
-define('ACCESS_TOKEN', '3424025068-CrqtCfBZW2nO7qJf906Kw63I0BLEymOxBTIWWqs');
-define('ACCESS_TOKEN_SECRET', 'iIln7v6dCqSqGMFoHxkrtBzBQJreCmhRSJqjKW2APpaJw');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('ACCESS_TOKEN', '');
+define('ACCESS_TOKEN_SECRET', '');
  
  //change count to chnage number of tweet to return
 $toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
